df94dae2639a696447e8513eed01d209961f03ec
anthgrim <anthgrim@icloud.com>
2022-02-13 21:40:53 -0500

    Multiplication Table JS Changes

--- multiplicationTables.js
+++ multiplicationTables.js
@@ -1,14 +1,26 @@
 //Tables exercise
 
 const multiplier = 1;
-let j = 1;
-let row = '';
+// let j = 1;
+//let row = '';
 
-for(let j = 1; j <= 10; j++){
-    for(let i = 1; i <= 10; i++){
+// for(let j = 1; j <= 10; j++){
+//     for(let i = 1; i <= 10; i++){
+//         let result = i * j;
+//         row += result + " ";    
+//     }
+//     console.log(row);
+//     row = '';
+// }
+
+for (let i = 1; i <= 10; i++) {
+
+    let row = '';
+
+    for(let j = 1; j <= 10; j++){
         let result = i * j;
-        row += result + " ";    
+        row += result + " ";
     }
+
     console.log(row);
-    row = '';
 }
